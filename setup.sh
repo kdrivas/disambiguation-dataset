6086ace8119ad14dee48de3735e73b2fb86d6c37
kervyrivas <k.rivas@pucp.pe>
2019-03-18 21:54:17 +0000

    update links

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cd data
 
 wget http://lcl.uniroma1.it/wsdeval/data/WSD_Evaluation_Framework.zip
 
-wget https://s3.amazonaws.com/frazada/disambiguation_light.zip
+https://s3.amazonaws.com/frazada/disambiguation.zip
 wget https://s3.amazonaws.com/frazada/serialize.zip
 wget https://s3.amazonaws.com/frazada/translation.zip
 
@@ -17,12 +17,12 @@ wget https://s3.amazonaws.com/frazada/environment.yml
 
 unzip WSD_Evaluation_Framework.zip
 unzip serialize.zip
-unzip disambiguation_light.zip
+unzip disambiguation.zip
 unzip translation.zip
 
 rm WSD_Evaluation_Framework.zip
 rm serialize.zip
-rm disambiguation_light.zip
+rm disambiguation.zip
 rm translation.zip
 
 # Activate environment
