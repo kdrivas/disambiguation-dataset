69f3accd05fe3dc18872ba9a3f75da85ed8ddd47
kervyrivas <krivas@pucp.pe>
2019-03-21 09:19:02 -0500

    more and more parameters

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,7 @@
 CUDA_IDS=$1
-MAX_LENGTH=200
+MAX_LENGTH=$2
 
+echo "Parameters " $CUDA_IDS
 python training.py --name_file=all_lemma --cuda=True --max_length=$MAX_LENGTH --cuda_ids=$CUDA_IDS &> all.txt
 
 python training.py --name_file=all_f1_lemma --cuda=True --max_length=$MAX_LENGTH --cuda_ids=$CUDA_IDS & > f1.txt
